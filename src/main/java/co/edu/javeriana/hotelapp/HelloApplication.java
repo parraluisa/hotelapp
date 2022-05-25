cbce5e70017aa5aa35e65077a86e908c44a4073c
SouthThunder <alejandrovemo@gmail.com>
2022-05-25 09:28:59 -0500

    create fxml file, and css link

--- src/main/java/co/edu/javeriana/hotelapp/HelloApplication.java
+++ src/main/java/co/edu/javeriana/hotelapp/HelloApplication.java
@@ -2,6 +2,7 @@ package co.edu.javeriana.hotelapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("test.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/co/edu/javeriana/hotelapp/HelloController.java
+++ src/main/java/co/edu/javeriana/hotelapp/HelloController.java
@@ -4,8 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+
     @FXML
     private Label welcomeText;
+
+    @FXML
+    protected void test()
+    {
+        System.out.println("fuck");
+    }
 
     @FXML
     protected void onHelloButtonClick() {
