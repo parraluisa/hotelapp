daa21e6a179b879747848b51311a942edd1c2071
parraluisa <parran_ll@javeriana.edu.co>
2022-05-25 09:12:11 -0500

    test commit

---
+++ src/main/java/co/edu/javeriana/hotelapp/test.java
@@ -0,0 +1,17 @@
+package co.edu.javeriana.hotelapp;
+
+public class test {
+    String holta;
+
+    public test(String holta) {
+        this.holta = holta;
+    }
+
+    public String getHolta() {
+        return holta;
+    }
+
+    public void setHolta(String holta) {
+        this.holta = holta;
+    }
+}
