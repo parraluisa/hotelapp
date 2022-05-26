50b277f0504f3de84797f2744aee11ec2f6da10e
parraluisa <parran_ll@javeriana.edu.co>
2022-05-26 12:38:00 -0500

    Arreglo de queries y arreglo de metodos en LogInDAO

--- src/main/java/co/edu/javeriana/hotelapp/model/dao/impl/LogInDAOImpl.java
+++ src/main/java/co/edu/javeriana/hotelapp/model/dao/impl/LogInDAOImpl.java
@@ -2,11 +2,10 @@ package co.edu.javeriana.hotelapp.model.dao.impl;
 
 import co.edu.javeriana.hotelapp.model.dao.LogInDAO;
 import co.edu.javeriana.hotelapp.model.dto.LogInDTO;
+import co.edu.javeriana.hotelapp.model.dto.PaisDTO;
 import co.edu.javeriana.hotelapp.util.Oracle;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +22,7 @@ public class LogInDAOImpl implements LogInDAO {
             this.oracle.conectar();
             String query="Insert into login_p2(usuario,contrasenia) values("
                     +"'"+login.getUser()+"',"
-                    +login.getPassword()+");";
+                    +login.getPassword()+")";
             Statement stmt = this.oracle.getConnection().createStatement();
             int code= stmt.executeUpdate(query);
             stmt.close();
@@ -31,7 +30,7 @@ public class LogInDAOImpl implements LogInDAO {
             switch (code) {
                 case 1:
                     System.out.println("Se creo el usuario");
-                    //   return findByName(login.getUser());
+                      return findByUser(login.getUser());
                 default:
                     return null;
             }
@@ -42,13 +41,12 @@ public class LogInDAOImpl implements LogInDAO {
 
         }
     }
-
     @Override
     public Boolean encontrarUsuario(String user, LogInDTO login) {
         Boolean res=false;
         try {
             this.oracle.conectar();
-            String query = "SELECT * FROM login_p2 WHERE user = '" + user + "';";
+            String query = "SELECT * FROM login_p2 WHERE user = '" + user + "'";
             System.out.println(query);
             Statement stmt = this.oracle.getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
             ResultSet rs = stmt.executeQuery(query);
@@ -70,6 +68,33 @@ public class LogInDAOImpl implements LogInDAO {
             return null;
         }
         return res;
+    }
+
+    @Override
+
+    public LogInDTO findByUser(String user){
+        try {
+            this.oracle.conectar();
+            String query = "SELECT * FROM login_p2 WHERE usuario = '" + user + "'";
+            System.out.println(query);
+            Statement stmt = this.oracle.getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.first()) {
+                LogInDTO log = new LogInDTO(
+                        rs.getString("usuario"),
+                        rs.getString("contrasenia"));
+                rs.close();
+                stmt.close();
+                return log;
+            } else {
+                rs.close();
+                stmt.close();
+                return null;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PaisDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
 
 }
